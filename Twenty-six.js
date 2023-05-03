ab75f7ce375fca84da3bc7c2249ce87d569e681d
Usha Rani <109506144+UshaRani-coder@users.noreply.github.com>
2023-05-03 23:29:49 +0530

    Add files via upload

---
+++ Twenty-six.js
@@ -0,0 +1,8 @@
+let countries = ['Albania','Belgium','UK','Finland','India',];
+if(countries.includes('Ethiopia')){
+    console.log('ETHIOPIA');
+}
+else{
+    countries.push('Ethiopia');
+    console.log(countries);
+}
