e62920a965d23ba36e9b6d946a416f672732b9f3
Usha Rani <109506144+UshaRani-coder@users.noreply.github.com>
2023-05-06 00:23:39 +0530

    Add files via upload

---
+++ Thirty-One.js
@@ -0,0 +1,4 @@
+let webTechs = ['HTML','CSS','JavaScript','Angular','Express.js','PHP'];
+let mernStack = ['MongoDB','Express','React','Node'];
+console.log(webTechs.sort());
+console.log(mernStack.sort());
